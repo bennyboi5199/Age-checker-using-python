c3bf4d95d608dffd854aa47a4bd0da9353919c80
piyush <piyushprasad816@gmail.com>
2020-08-25 07:10:09 -0700

    Initial commit

---
+++ age .py
@@ -0,0 +1,28 @@
+import time
+
+def age(n):
+	year = time.strftime("%Y")
+	currentyear = int(year)
+	for i in range(currentyear,currentyear+100):
+		print(f"your age in {i} is {i-n}")
+
+
+
+if __name__ == '__main__':
+
+	useryear = int(input("Enter your birth year: "))
+	year = time.strftime("%Y")
+	currentyear = int(year)
+
+	if  useryear > currentyear:
+		print(f"sorry {useryear} this year not come yet")
+
+	elif useryear == currentyear:
+		print("you are a new born baby")
+	
+	else:
+		for i in range(currentyear,currentyear+100):
+			print(f"your age in {i} is {i-useryear}")
+
+
+	input()
